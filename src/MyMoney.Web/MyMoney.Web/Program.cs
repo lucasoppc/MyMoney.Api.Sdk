22bfb29da005c3f51585b1d671deba4a516bf65d
lucashernandez <matiasoppc@icloud.com>
2024-06-05 23:54:01 -0300

    remove blazor bootstrap package

--- src/MyMoney.Web/MyMoney.Web/Program.cs
+++ src/MyMoney.Web/MyMoney.Web/Program.cs
@@ -9,7 +9,6 @@ using MyMoney.Web.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
