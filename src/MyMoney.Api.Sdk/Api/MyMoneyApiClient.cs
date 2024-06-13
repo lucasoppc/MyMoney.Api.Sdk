3598b287c7bf83f9358be8c6c0a1986d74b20df5
lucashernandez <matiasoppc@icloud.com>
2024-06-12 22:40:52 -0300

    expose transactions api

--- src/MyMoney.Api.Sdk/Api/MyMoneyApiClient.cs
+++ src/MyMoney.Api.Sdk/Api/MyMoneyApiClient.cs
@@ -2,8 +2,10 @@
 namespace MyMoney.Api.Sdk.Api;
 
 public class MyMoneyApiClient(Accounts.Accounts  accounts,
-    Users.Users users)
+    Users.Users users,
+    Transactions.Transactions transactions)
 {
     public Accounts.Accounts Accounts => accounts;
     public Users.Users Users => users;
+    public Transactions.Transactions Transactions => transactions;
 }
